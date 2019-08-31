1a55c8348af45d6bc9ecd9263dc2ffe44f289403
2Bbear <wkdwjdgh1234@naver.com>
2019-08-31 15:55:17 +0900

    라우트 코드 추가

--- Source/expressSampleServer/dikaoWebServer.js
+++ Source/expressSampleServer/dikaoWebServer.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+
 var server = app.listen(8080, function(){
     console.log("Express server has started on port 8080")
+})
+
+app.get('/', function(req, res)
+{
+  res.send('hello world2');
 })
