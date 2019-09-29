fc413b38fc8e4e1aa68076d015e2efaf6799462d
Jangjungho <wkdwjdgh1234@naver.com>
2019-09-29 18:34:40 +0900

    카카오 코드 삭제
    주석처리 해놓음

--- Source/expressSampleServer/dikaoWebServer.js
+++ Source/expressSampleServer/dikaoWebServer.js
@@ -1,49 +1,49 @@
 //const request = require('request');
-var express = require('express');
-var app = express();
-const passport = require('passport');
-const KakaoStrategy = require('passport-kakao').Strategy;
+// var express = require('express');
+// var app = express();
+// const passport = require('passport');
+// const KakaoStrategy = require('passport-kakao').Strategy;
 const config = require('../../TargetConfig');
 
 //kakao API code===================================================================
-passport.use('login-kakao', new KakaoStrategy({
-  clientID : config.kakao.client_id,
-  callbackURL : config.kakao.callback_url_redirect // 카카오 개발자 사이트에서 지정한 리다이렉트 URL 
-},
-  function(accessToken, refreshToken, profile, done) {
-    console.log(profile);
-    return done(null, profile);
-  }
-));
+// passport.use('login-kakao', new KakaoStrategy({
+//   clientID : config.kakao.client_id,
+//   callbackURL : config.kakao.callback_url_redirect // 카카오 개발자 사이트에서 지정한 리다이렉트 URL 
+// },
+//   function(accessToken, refreshToken, profile, done) {
+//     console.log(profile);
+//     return done(null, profile);
+//   }
+// ));
 
-let template_objectObj = {
-  object_type: 'text',
-  text: ' Hello Kakao!(텍스트 영역입니다. 최대 200자 표시 가능합니다.)',
-  'link': {
-    web_url: 'https://developers.kakao.com',
-    mobile_web_url: 'https://developers.kakao.com'
-  }
-};
+// let template_objectObj = {
+//   object_type: 'text',
+//   text: ' Hello Kakao!(텍스트 영역입니다. 최대 200자 표시 가능합니다.)',
+//   'link': {
+//     web_url: 'https://developers.kakao.com',
+//     mobile_web_url: 'https://developers.kakao.com'
+//   }
+// };
 // Javascript -> JSON 타입으로 변경
-let template_objectStr = JSON.stringify(template_objectObj);
-let options = {
-    url: 'https://kapi.kakao.com/v2/api/talk/memo/default/send',
-    method: 'POST',
-    headers: {
-    'Authorization': 'Bearer xxxxxxxxxxxxxxxxxxxxxx',
-        'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  form: {
-    template_object: template_objectStr,
-  }
-};
+// let template_objectStr = JSON.stringify(template_objectObj);
+// let options = {
+//     url: 'https://kapi.kakao.com/v2/api/talk/memo/default/send',
+//     method: 'POST',
+//     headers: {
+//     'Authorization': 'Bearer xxxxxxxxxxxxxxxxxxxxxx',
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//   },
+//   form: {
+//     template_object: template_objectStr,
+//   }
+// };
 
-function callback(error, response, body) {
-  console.log(response.statusCode);
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-    }
-}
+// function callback(error, response, body) {
+//   console.log(response.statusCode);
+//     if (!error && response.statusCode == 200) {
+//         console.log(body);
+//     }
+// }
 
 
 //Dikao code========================================================================
