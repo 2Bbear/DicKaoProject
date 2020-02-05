4afa3f504a04d8cf2fe9f03b9b03dc6086a14a4c
jungho <jjh.twobbear@gmail.com>
2020-02-05 15:52:45 +0900

    상태값 관리하는 코드 추가 완료

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const client = new Discord.Client();
 
 
 //파일 시스템 모듈 사용
-var curStateDatas;
+var curStateDatas= new Array();
 var curStateDataIndex;
 
 
 var Initializing = function()
 {
-  curStateDatas = new Array();
+  curStateDatas.push("공부중","작업중","수면중","게임중");
+  curStateDataIndex=2;
 }
 
 
@@ -23,39 +24,106 @@ client.on('ready', () => {
   
 });
 
+function is_number(v) {
+  var reg = /^(\s|\d)+$/;
+  return reg.test(v);
+}
+
 
 //주 컨텐츠 영역 ======================================================================================================
 client.on('message', message => {
   if (message.channel.type == 'dm') return //direct message
   if (message.content.startsWith(config.prefix))
   {
-    test = message.toString();
+    var messageText = message.toString();
     //상태값 추가 커맨드
-    if(message.content.includes(config.prefix + 'addst'))
+    if(message.content.startsWith(config.prefix + 'addst'))
     {
       message.channel.send('상태값 추가 커맨드');
+      var strArr = messageText.split(config.prefix +'addst');
+    
+      var stateName =  strArr[1];
+      curStateDatas.push(stateName);
+      message.channel.send(stateName+'을 추가하였습니다');
     }
     //상태값 지정 커맨드
-    if(message.content.includes(config.prefix + 'cst'))
+    if(message.content.startsWith(config.prefix + 'cst'))
     {
       message.channel.send('상태값 지정 커맨드');
+      var strArr = messageText.split(config.prefix +'cst');
+
+      var number =  Number(strArr[1]);
+      //만약 숫자 값이 아닌경우
+      if(is_number(number)==false)
+      {
+        message.channel.send('숫자를 입력해주세요');
+        return;
+      }
+      //해당 숫자가 상태 리스트 값 안에 존재하지 않을 경우
+      if(0>number ||curStateDatas.length<=number)
+      {
+        message.channel.send('리스트에서 벗어난 값입니다.');
+        return;
+      }
+      //현재 상태 값을 변경
+      curStateDataIndex = number;
+      message.channel.send(curStateDatas[curStateDataIndex]+'로 현재상태를 변경하였습니다.');
     }
     //상태값 삭제 커맨드
-    if(message.content.includes(config.prefix + 'delst'))
+    if(message.content.startsWith(config.prefix + 'delst'))
     {
       message.channel.send('상태값 삭제 커맨드');
+      var strArr = messageText.split(config.prefix +'delst');
+
+      var number =  Number(strArr[1]);
+      //만약 숫자 값이 아닌경우
+      if(is_number(number)==false)
+      {
+        message.channel.send('숫자를 입력해주세요');
+        return;
+      }
+      //해당 숫자가 상태 리스트 값 안에 존재하지 않을 경우
+      if(0>number ||curStateDatas.length<number)
+      {
+        message.channel.send('리스트에서 벗어난 값입니다.');
+        return;
+      }
+
+      var tempStateName=new String();
+      //해당 문자열 삭제
+      if(number>-1)
+      {
+        tempStateName = curStateDatas[number];
+        curStateDatas.splice(number,1);
+      }
+
+      message.channel.send(tempStateName+'를 삭제했습니다.');
+
     }
     //상태값 전부 삭제 커맨드
-    if(message.content.includes(config.prefix + 'alldelst'))
+    if(message.content.startsWith(config.prefix + 'alldelst'))
     {
       message.channel.send('상태값 전부 삭제 커맨드');
+      curStateDatas=[];
+      message.channel.send('모든 상태값을 삭제하였습니다.');
     }
     //상태값 출력 커맨드
-    if(message.content.includes(config.prefix + 'stlist'))
+    if(message.content.startsWith(config.prefix + 'stlist'))
     {
-      message.channel.send('상태값 출력 커맨드');
+      var printOutText=new String();
+      printOutText = '현재 선택 가능한 상태 값';
+     
+      curStateDatas.forEach(function (item, index, Array){
+        printOutText= printOutText+'\n'+String(index)+" : "+item;
+      });
+
+      message.channel.send(printOutText);
     }
-    
+    //현재 상태값을 출력하는 커맨드
+    if(message.content.startsWith(config.prefix + 'curst'))
+    {
+      message.channel.send("현재 상태 값은 "+ curStateDatas[curStateDataIndex] + ' 입니다.');
+    }
   } 
   else
   {
@@ -66,8 +134,6 @@ client.on('message', message => {
     }
     return
   }
-  
-
 });
 client.login(config.token);
 
