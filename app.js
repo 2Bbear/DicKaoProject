f76de95838788198f6e36ce4a0c3c05357566682
jungho <jjh.twobbear@gmail.com>
2020-02-08 14:01:15 +0900

    음성보이스 추가 성공적

--- app.js
+++ app.js
@@ -1,42 +1,153 @@
 const Discord = require('discord.js');
 const config = require('./TargetConfig');
-//const POEAPI = require('./source/PathOfExileAPI');
 const client = new Discord.Client();
 
-
-
+const ytdl = require('ytdl-core');
+const queue = new Map();
 
 //파일 시스템 모듈 사용
 var curStateDatas= new Array();
 var curStateDataIndex;
 
 
+//함수들==============================================================================================
 var Initializing = function()
 {
   curStateDatas.push("공부중","작업중","수면중","게임중");
   curStateDataIndex=2;
 }
 
+//이게 숫자인지 아닌지 판단하는 메소드
+function is_number(v) {
+  var reg = /^(\s|\d)+$/;
+  return reg.test(v);
+}
 
+async function execute(message, serverQueue) {
+	const args = message.content.split(' ');
+
+	const voiceChannel = message.member.voiceChannel;
+	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+		return message.channel.send('I need the permissions to join and speak in your voice channel!');
+	}
+
+	const songInfo = await ytdl.getInfo(args[1]);
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+	};
+
+	if (!serverQueue) {
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+
+		queue.set(message.guild.id, queueContruct);
+
+		queueContruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueContruct.connection = connection;
+			play(message.guild, queueContruct.songs[0]);
+		} catch (err) {
+			console.log(err);
+			queue.delete(message.guild.id);
+			return message.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`${song.title} has been added to the queue!`);
+	}
+
+}
+
+function skip(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+	serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', () => {
+			console.log('Music ended!');
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+//기본 reply메세지==================================================================================
 client.on('ready', () => {
   console.log('서비스 시작!');
   //초기화
   Initializing();
   
 });
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
+});
 
-function is_number(v) {
-  var reg = /^(\s|\d)+$/;
-  return reg.test(v);
-}
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
+
+
 
 
 //주 컨텐츠 영역 ======================================================================================================
 client.on('message', message => {
   if (message.channel.type == 'dm') return //direct message
+  if (message.author.bot) return;
+  if (!message.content.startsWith(config.prefix)) return;
+
+  const serverQueue = queue.get(message.guild.id);
+
   if (message.content.startsWith(config.prefix))
   {
-    var messageText = message.toString();
+    //=============================================================\
+    if(message.content.startsWith(`${config.prefix}play`))
+    {
+      execute(message, serverQueue);
+		  return;
+    }
+    else if(message.content.startsWith(`${config.prefix}skip`))
+    {
+      skip(message, serverQueue);
+		  return;
+    }
+    else if(message.content.startsWith(`${config.prefix}stop`))
+    {
+      stop(message, serverQueue);
+		  return;
+    }
+    
+    //=============================================================
     //상태값 추가 커맨드
     if(message.content.startsWith(config.prefix + 'addst'))
     {
@@ -46,6 +157,7 @@ client.on('message', message => {
       var stateName =  strArr[1];
       curStateDatas.push(stateName);
       message.channel.send(stateName+'을 추가하였습니다');
+      return;
     }
     //상태값 지정 커맨드
     if(message.content.startsWith(config.prefix + 'cst'))
@@ -69,6 +181,7 @@ client.on('message', message => {
       //현재 상태 값을 변경
       curStateDataIndex = number;
       message.channel.send(curStateDatas[curStateDataIndex]+'로 현재상태를 변경하였습니다.');
+      return;
     }
     //상태값 삭제 커맨드
     if(message.content.startsWith(config.prefix + 'delst'))
@@ -99,6 +212,7 @@ client.on('message', message => {
       }
 
       message.channel.send(tempStateName+'를 삭제했습니다.');
+      return;
 
     }
     //상태값 전부 삭제 커맨드
@@ -107,6 +221,7 @@ client.on('message', message => {
       message.channel.send('상태값 전부 삭제 커맨드');
       curStateDatas=[];
       message.channel.send('모든 상태값을 삭제하였습니다.');
+      return;
     }
     //상태값 출력 커맨드
     if(message.content.startsWith(config.prefix + 'stlist'))
@@ -119,12 +234,16 @@ client.on('message', message => {
       });
 
       message.channel.send(printOutText);
+      return;
     }
     //현재 상태값을 출력하는 커맨드
     if(message.content.startsWith(config.prefix + 'curst'))
     {
       message.channel.send("현재 상태 값은 "+ curStateDatas[curStateDataIndex] + ' 입니다.');
+      return;
     }
+
+    //===================================================================================================
 
     //전송한 메세지 콘솔에 띄우는 법
     if(message.content===(config.prefix + 'tm1'))
@@ -134,7 +253,7 @@ client.on('message', message => {
       .catch(console.error);
     }
     //url 링크 파일 전송하는 법
-    if(message.content===(config.prefix + 'tm2'))
+    else if(message.content===(config.prefix + 'tm2'))
     {
       message.channel.send({
         files: ['http://blogattach.naver.net/d045cc7f6d3734e8c72a437b48a8d7a8025aa2427e/20200203_137_blogfile/harne__1580667756631_QYEa3d_smi/%5BOhys-Raws%5D+ID+Invaded+-+06+%28BS11+1280x720+x264+AAC%29.smi?type=attachment']})
@@ -142,7 +261,7 @@ client.on('message', message => {
         .catch(console.error);
     }
     //로컬 파일 전송하는 방법
-    if(message.content===(config.prefix + 'tm3'))
+    else if(message.content===(config.prefix + 'tm3'))
     {
       message.channel.send({
         files: [{
@@ -153,7 +272,7 @@ client.on('message', message => {
         .catch(console.error);
     }
     //로컬 이미지를 임베디드해서 넣는 방법
-    if(message.content===(config.prefix + 'tm4'))
+    else if(message.content===(config.prefix + 'tm4'))
     {
       message.channel.send('This is an embed', {
         embed: {
@@ -170,21 +289,20 @@ client.on('message', message => {
         .then(console.log)
         .catch(console.error);
     }
-
     //채널의 이름 변경 방법 (관리자 권한을 요구함)
-    if(message.content===(config.prefix + 'tm5'))
+    else if(message.content===(config.prefix + 'tm5'))
     {
       message.channel.setName('not_general')
       .then(newChannel => console.log(`Channel's new name is ${newChannel.name}`))
       .catch(console.error);
     }
     //후방 금지 채널로 지정하는 방법
-    if(message.content===(config.prefix + 'tm6'))
+    else if(message.content===(config.prefix + 'tm6'))
     {
       message.channel.setNSFW(true,'there is no reason');
     }
     //카테고리 변경 방법
-    if(message.content===(config.prefix + 'tm7'))
+    else if(message.content===(config.prefix + 'tm7'))
     {
       //첫번째 매개변수에 카테고리의 id 값을 입력해야 합니다.
       message.channel.setParent('674872026997063680', { lockPermissions: false })
@@ -192,7 +310,7 @@ client.on('message', message => {
       .catch(console.error);
     }
     //메세지를 삭제하는 방법 , 현재부터 2주 전까지의 메세지를 일괄 삭제하는 방법
-    if(message.content===(config.prefix + 'tm8'))
+    else if(message.content===(config.prefix + 'tm8'))
     {
       //첫번째 매개변수에 카테고리의 id 값을 입력해야 합니다.
       message.channel.bulkDelete(5) // 지우고 싶은 메세지 숫자를 입력해요
@@ -200,7 +318,7 @@ client.on('message', message => {
       .catch(console.error);
     }
     //초대코드 만드는 방법
-    if(message.content===(config.prefix + 'tm9'))
+    else if(message.content===(config.prefix + 'tm9'))
     {
       message.channel.createInvite({
         temporary :true, // 24동안 역할을 받지 못하면 바로 나가게 하는 초대권을 만들 것인지
@@ -211,7 +329,7 @@ client.on('message', message => {
       .catch(console.error);
     }
     //특정 단어를 감지하게 하는 방법
-    if(message.content===(config.prefix + 'tm10'))
+    else if(message.content===(config.prefix + 'tm10'))
     {
       //
       const filter = m => m.content.includes('discord');
@@ -220,7 +338,7 @@ client.on('message', message => {
       collector.on('end', collected => console.log(`Collected ${collected.size} items`));
     }
     //해당 채널에 있는 사람들에게 역할이나 권한을 덮어씌우는 방법
-    if(message.content===(config.prefix + 'tm11'))
+    else if(message.content===(config.prefix + 'tm11'))
     {
       message.channel.createOverwrite(message.author, {
         SEND_MESSAGES: false
@@ -229,7 +347,7 @@ client.on('message', message => {
         .catch(console.error);
     }
     //웹훅 만드는 방법
-    if(message.content===(config.prefix + 'tm12'))
+    else  if(message.content===(config.prefix + 'tm12'))
     {
       // Create a webhook for the current channel
       message.channel.createWebhook('Snek', 
@@ -240,7 +358,7 @@ client.on('message', message => {
         .catch(console.error)
     }
     //채널 삭제
-    if(message.content===(config.prefix + 'tm13'))
+    else if(message.content===(config.prefix + 'tm13'))
     {
       // Delete the channel
       message.channel.delete('making room for new channels')
@@ -248,7 +366,7 @@ client.on('message', message => {
       .catch(console.error);
     }
     //채널 수정
-    if(message.content===(config.prefix + 'tm14'))
+    else if(message.content===(config.prefix + 'tm14'))
     {
       // Edit a channel
       message.channel.edit({
@@ -268,16 +386,15 @@ client.on('message', message => {
       .catch(console.error);  
     }
     //채널 주제 설정
-    if(message.content===(config.prefix + 'tm15'))
+    else if(message.content===(config.prefix + 'tm15'))
     {
       // Set a new channel topic
       message.channel.setTopic('needs more rate limiting')
       .then(newChannel => console.log(`Channel's new topic is ${newChannel.topic}`))
       .catch(console.error);
     }
-
     //~~님이 입력하고 있습니다..를 띄움..뭐하는 용도지 이거
-    if(message.content===(config.prefix + 'tm16'))
+    else if(message.content===(config.prefix + 'tm16'))
     {
       // Start typing in a channel with a typing count of five, or set it to five
       message.channel.startTyping(5);
@@ -285,29 +402,6 @@ client.on('message', message => {
       channel.stopTyping(true); //이거 해야 타이빙합니다~~ 하는게 끝남
     }
 
-    //voice 채널에 봇을 초대하는 방법
-    if(message.content===(config.prefix + 'tm17'))
-    {
-      const channel = message.channel.id;
-      if (!channel) return console.error("The channel does not exist!");
-      channel.chal
-      .then(connection => {
-        // Yay, it worked!
-        console.log("Successfully connected.");
-      }).catch(e => {
-        // Oh no, it errored! Let's log it to console :)
-        console.error(e);
-      });
-      // if(message.member.voiceChannel)
-      // {
-      //   message.member.voiceChannel.join()
-      //     .then(connection => console.log('Connected!'));
-      // }
-      // else{
-      //   message.reply('You must be in a voice channel to summon me!');
-      // }
-      
-    }
   } 
   else
   {
@@ -316,9 +410,8 @@ client.on('message', message => {
     {
       message.channel.send('TwoBbearX_X님은 현재 '+curStateDatas[curStateDataIndex]+' 입니다');
     }
-
     //아바타 예제
-    if(message.content === 'what is my avatar')
+    else if(message.content === 'what is my avatar')
     {
       message.channel.send(message.author.displayAvatarURL);
     }
@@ -329,7 +422,9 @@ client.on('message', message => {
     //   // Send the attachment in the message channel
     //   message.channel.send(attachment);
     // }
-    return
+
+    message.channel.send('You need to enter a valid command!');
+    return;
   }
 });
 //새로운 멤버가 들어왔을때
@@ -340,6 +435,26 @@ client.on('guildMemberAdd',member=>{
   
   channel.send(`이 서버에 오신걸 환영합니다., ${member}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(config.token);
 
 /*
